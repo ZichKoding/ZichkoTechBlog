a4158d560bf0ba50deea4a1a58fe1b34c6ec390b
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-24 10:59:55 -0500

    Added User model and routes for GET all and POST. Added password encryption as well

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
-// const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// activate routes
+app.use(routes);
 
 
 // turn on connection to db and server 
