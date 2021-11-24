fbc56d6238942e0eca0f49712c854e26c4b2002a
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-24 18:04:40 -0500

    Added GET by id, PUT, and DELETE methods to the User routes.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,6 +12,27 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id,
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 
 router.post('/', (req, res) => {
     User.create({
@@ -20,6 +41,47 @@ router.post('/', (req, res) => {
         password: req.body.password
     })
         .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: "No user found with this id." });
+                return;
+            }
+
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user found with this id." });
+                return;
+            }
+
+            res.json(dbUserData);
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
