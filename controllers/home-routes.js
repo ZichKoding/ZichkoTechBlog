2126b0f46b3ac8b153e1aab4c52929f238df22f7
Chris Zichko <chriszichkocoding@gmail.com>
2021-11-30 07:50:55 -0500

    Where I left off on Thursday

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Post } = require('../../just-tech-news/models');
 const sequelize = require('../config/connection');
 const { Blog, User, Comment } = require('../models');
 
@@ -17,7 +18,7 @@ router.get('/', (req, res) => {
             },
             {
                 model: Comment,
-                attributes: [ 'id', 'comment', 'user_id', 'blog_id' ],
+                attributes: [ 'comment', 'created_at' ],
                 include: [
                     {
                         model: User,
@@ -31,6 +32,52 @@ router.get('/', (req, res) => {
             const blogs = dbBlogData.map(blog => blog.get({ plain: true }));
             res.render('homepage', {
                 blogs
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/post/:id', (req, res) => {
+    Blog.findOne({
+        attributes: [
+            'id',
+            'title',
+            'blog_post',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['comment', 'created_at'],
+                include: [
+                    {
+                        model: User,
+                        attributes: ['username']
+                    }
+                ]
+            }
+        ]
+    })
+        .then(dbBlogData => {
+            if (!dbBlogData) {
+                res.status(404).json({ message: "No post found with this id." });
+                return;
+            }
+            // serialize the data
+            const blog = dbBlogData.get({ plain: true });
+
+            console.log(blog);
+
+            // pass data to template
+            res.render('single-blog', {
+                blog
             });
         })
         .catch(err => {
