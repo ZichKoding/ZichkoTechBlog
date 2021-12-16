29a1022872a836a796b648134575168572d2d97f
Chris Zichko <chriszichkocoding@gmail.com>
2021-12-16 11:29:28 -0500

    Dashboard is now loading.

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Blog, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    // find all blogs that the user owns
+    Blog.findAll({
+        where: {
+            // user the user id from the session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'blog_post',
+            'created_at'
+        ],
+        // Add the username to the blog
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            // add any comments related to the blog
+            {
+                model: Comment,
+                attributes: [ 'id', 'comment', 'created_at', 'user_id', 'blog_id' ],
+                include: [
+                    {
+                        model: User,
+                        attributes: ['username']
+                    }
+                ]
+            }
+        ]
+    })
+        .then(dbBlogData => {
+            // serialize the data before passing handlebars
+            const blogs = dbBlogData.map(blog => blog.get({ plain: true }));
+            res.render('dashboard', { blogs, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
